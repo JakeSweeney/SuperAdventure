b790edff46e1704717207c93b1493c2078c96b85
JakeSweeney <jake.sweeney.03@gmail.com>
2023-08-17 15:18:04 +0100

    WIP; initial project setup

--- SuperAdventure/Properties/Resources.Designer.cs
+++ SuperAdventure/Properties/Resources.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace Engine.Properties
+namespace SuperAdventure.Properties
 {
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
@@ -43,7 +43,7 @@ namespace Engine.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Engine.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SuperAdventure.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- SuperAdventure/SuperAdventure.Designer.cs
+++ SuperAdventure/SuperAdventure.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace Engine
+namespace SuperAdventure
 {
     partial class SuperAdventure
     {
